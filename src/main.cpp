a41d0f9d3d47a5079f643c56ba622d77c34685fb
Juan Perez <jpablopm72@gmail.com>
2023-05-23 14:17:03 -0400

    Added some info

---
+++ src/main.cpp
@@ -0,0 +1,12 @@
+//
+// Created by juperez on 5/23/23.
+//
+#include <iostream>
+#include <tbb/parallel_for.h>
+#include <tbb/blocked_range.h>
+#include <tbb/task_scheduler_observer.h>
+
+int main() {
+    std::cout << "Here \n";
+    return 0;
+}
